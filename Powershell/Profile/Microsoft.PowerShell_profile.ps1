07bea4af6ce5b78d94e7fedbf1472519da5f1d84
john_hagler_cp <SND\john_hagler_cp>
2015-03-13 18:50:54 +0000

    Added the D2D_PSFunctions.ps1 script and reference in profile.

--- Powershell/Profile/Microsoft.PowerShell_profile.ps1
+++ Powershell/Profile/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,3 @@
 ."C:\Program Files\Microsoft Dynamics AX\60\ManagementUtilities\Microsoft.Dynamics.ManagementUtilities.ps1"
+
+."C:\VisualStudioOnline\Workspaces\CodePlex\Day2DayDynamics\Powershell\StandAloneFunctions\D2D_PSFunctions.ps1"
