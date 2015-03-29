2864807b567e62e4147aa16290ca152dca694f4d
john_hagler_cp <32314660+johnkhagler@users.noreply.github.com>
2015-03-29 19:50:22 +0000

    Added the Get-AXAutoRunXML to the stand alone functions. Updated the D2D_PSFunctions script to include the Get-AXAutoRunXML function.

---
+++ Powershell/StandAloneFunctions/Function_Get-AXAutoRunXML.ps1
@@ -0,0 +1,47 @@
+﻿function Get-AXAutoRunXML{
+###########################################################################################################################################################
+#.Synopsis
+#  Creates an AxaptaAutoRun xml string.
+#.Description
+#  Creates an AxaptaAutoRun xml string.
+#.Example
+#  Get-AXAutoRunXML -$Command 'Synchronize'
+#.Example
+#  Get-AXAutoRunXML -$Command 'CompileApplication' -LogFile 'C:\TestLog.log' -ExitWhenDone
+#.Parameter ExitWhenDone
+#  Tells AX to close when the command is complete.
+#.Parameter -LogFile
+#  Tells AX whether to use a log file and where to put it.
+#.Parameter -$Command
+#  Tells AX what autorun command to use.
+###########################################################################################################################################################
+[CmdletBinding()]
+param(
+    [Parameter(ValueFromPipeline = $True)]
+    [Switch]$ExitWhenDone,
+    [Parameter(ValueFromPipeline = $True)]
+    [String]$LogFile,
+    [Parameter(ValueFromPipeline = $True)]
+    [ValidateNotNullOrEmpty()]
+    [String]$Command
+)
+   
+    $XMLHeader = '<?xml version="1.0" encoding="utf-8"?>'
+    $AutoRunHeader = '<AxaptaAutoRun version="4.0"{0}>'
+    $AutoRunCommand = '<{0} />' -f $Command
+    $AutoRunFooter = '</AxaptaAutoRun>'
+
+    if (![String]::IsNullOrWhiteSpace($LogFile))
+    {
+       $ARHInsert += ' logFile="{0}"' -f $LogFile
+    }
+
+    if ($ExitWhenDone)
+    {
+       $ARHInsert += ' exitWhenDone="true"'
+    }
+
+    $XML = $XMLHeader + ($AutoRunHeader -f $ARHInsert) + $AutoRunCommand + $AutoRunFooter
+
+    $XML.ToString()
+}
