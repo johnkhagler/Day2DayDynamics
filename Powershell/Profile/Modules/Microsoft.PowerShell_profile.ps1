926784e116a888186e7c38a1b20b2a7debebd2d9
john_hagler_cp <SND\john_hagler_cp>
2015-09-01 01:57:47 +0000

    Added D2DDynamics custom PowerShell module.  Created hierarchy for profiles and added the new module profile.

---
+++ Powershell/Profile/Modules/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+#AX 2012 Management Shell reference
+."C:\Program Files\Microsoft Dynamics AX\60\ManagementUtilities\Microsoft.Dynamics.ManagementUtilities.ps1"
+
+#Load Modules
+Import-Module DynamicsAXCommunity -DisableNameChecking #DynamicsAXCommunity module
+
+Import-Module D2DDynamics -DisableNameChecking #D2DDynamics module
+
+Push-Location
+Import-Module SQLPS -DisableNameChecking #SQL Powershell Module
+Pop-Location
+
+#Team Foundation Server References
+$env:path += ";C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\" #TF.exe
